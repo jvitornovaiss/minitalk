2e7a3f989be5730dac0c8cd995e5a67871fe534f
Joao Vitor Novais <jnovais@c2r5p5.42sp.org.br>
2025-12-21 15:05:33 -0300

    new comparative

---
+++ new/include/minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnovais <jnovais@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/10 15:54:18 by jnovais          #+#    #+#             */
+/*   Updated: 2025/10/10 17:16:15 by jnovais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../ft_printf/ft_printf.h"
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/types.h>
+
+#endif
